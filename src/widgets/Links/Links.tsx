ca9aba9bfb93d0c2072a9c673134acb4b6b7c57c
iufb <misakilover228@gmail.com>
2024-07-24 15:11:51 +0500

    styles: links widget

--- src/widgets/Links/Links.tsx
+++ src/widgets/Links/Links.tsx
@@ -6,9 +6,12 @@ export const Links = ({ title, items }: LinksProps) => {
   return (
     <section className="flex flex-col gap-3">
       <h2 className="text-xl text-cyan-500 font-bold">{title}</h2>
-      <ul className="transition-colors duration-150 flex flex-col gap-3 underline hover:underline-offset-2 hover:decoration-cyan-500">
+      <ul className="transition-colors duration-150 flex flex-col gap-3 ">
         {items.map((li, idx) => (
-          <li key={idx} className="hover:text-cyan-500 text-xl ">
+          <li
+            key={idx}
+            className="hover:text-cyan-500 text-xl underline hover:underline-offset-2 hover:decoration-cyan-500 "
+          >
             <a href={li.link} target="_blank">
               {li.name}
             </a>
