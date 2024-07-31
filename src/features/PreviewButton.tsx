55868692b08fe417c3c837643da4810bd749bd77
iufb <bashirov3ld2@gmail.com>
2024-07-31 16:13:33 +0500

    fix: preview button styles

--- src/features/PreviewButton.tsx
+++ src/features/PreviewButton.tsx
@@ -12,6 +12,14 @@ interface PreviewButtonProps {
   modal: string;
 }
 const mockProps = {
+  Info: {
+    options:
+      '{"title":"We prepare today’s learners for tomorrow’s world of work","content":"Our mission is to provide people with access to digitally-enhanced capacity development services to successfully manage their future of work transitions. From our sustainability-minded courses to our SDG-informed Masters, we always put our participants first.","items":[{"title":"Hundreds of courses","content":"The Centre leverages global alliances and partnerships to deliver training activities.  Courses cover topics like employment promotion, international labour standards, social protection, social dialogue, innovation, gender equality and diversity, sustainable development, and the future of work.  We offer academies, standard courses, free self-paced courses, and Masters. You can also search by topic, language, and mode (online, face-to-face, or blended).","image":"uploads/20240725110251.jpeg","imagePosition":"right","linkText":"See more","savedTemplate":"Main-carousel","templateWidgets":"[\\"Text\\",\\"Carousel\\"]","href":"/template-1721905318844","templateSlug":"/template-1721905318844","templateId":"633*634"}]}',
+  },
+  Links: {
+    options:
+      '{"title":"Нормативные правовые акты","items":[{"name":"Трудовой кодекс Республики Казахстан","link":"https://adilet.zan.kz/rus/docs/K1500000414","image":"","href":"/template-1721815109111","templateId":"574*575","templateName":null},{"name":"Закон РК \\"О профессиональных союзах\\"","link":"https://adilet.zan.kz/rus/docs/Z1400000211","image":"","href":"/template-1721815182890","templateId":"576*577","templateName":null}]}',
+  },
   Cards: {
     options:
       '{"variant":"horizontal","title":"123","items":[{"title":"123","content":"123","image":"uploads/20240729115725.webp","href":"","templateSlug":"/template-1722254220633","templateId":"679*680"},{"title":"123","content":"123","image":"uploads/20240730103228.jpeg","href":"","templateSlug":"/template-1722335539079","templateId":"683*684"}]}',
@@ -52,10 +60,12 @@ export const PreviewButton = ({ modal }: PreviewButtonProps) => {
             Здесь вы можете посмотреть как выглядит виджет
           </DialogDescription>
         </DialogHeader>
-        {getWidgetByName(
-          modal,
-          JSON.parse(mockProps[modal as keyof typeof mockProps].options),
-        )}
+        <div className="max-w-[1200px] w-full mx-auto">
+          {getWidgetByName(
+            modal,
+            JSON.parse(mockProps[modal as keyof typeof mockProps].options),
+          )}
+        </div>
       </DialogContent>
     </Dialog>
   );
