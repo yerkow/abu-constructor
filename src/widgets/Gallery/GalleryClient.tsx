23d639eef8d439bc7b2810a602fcdc9cbeb46b48
iufb <bashirov3ld2@gmail.com>
2024-07-31 15:02:02 +0500

    fix: gallery widget

--- src/widgets/Gallery/GalleryClient.tsx
+++ src/widgets/Gallery/GalleryClient.tsx
@@ -1,5 +1,13 @@
 "use client";
 import { backendImageUrl } from "@/shared/lib/constants";
+import {
+  Dialog,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogContent,
+} from "@/shared/ui";
 import clsx from "clsx";
 import Image from "next/image";
 import { useState } from "react";
@@ -10,37 +18,55 @@ export interface GalleryProps {
 interface GalleryItem {
   image: string;
 }
+
 function GalleryClient({ items }: GalleryProps) {
   const [selected, setSelected] = useState(0);
   return (
     <div className="grid gap-4 justify-center w-full">
-      <div className="flex relative w-[290px] h-[290px] md:w-[400px] md:h-[400px] justify-self-center">
-        <Image
-          className="rounded-lg"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          src={`${backendImageUrl}/${items[selected].image}`}
-          alt=""
-          fill
-        />
-      </div>
-      <div className="grid flex-nowrap  grid-cols-gallery lg:grid-cols-4 gap-4 overflow-x-auto">
+      <div className="grid flex-nowrap   grid-cols-gallery lg:grid-cols-4 gap-4 overflow-x-auto">
         {items.map((item, idx) => (
-          <div
-            onClick={() => setSelected(idx)}
-            key={idx}
-            className={clsx(
-              "relative cursor-pointer w-[180px] h-[180px]  md:h-[200px] md:w-[200px] rounded-md bg-slate-50",
-              idx == selected ? "hidden" : "block",
-            )}
-          >
-            <Image
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className="w-full   h-full object-cover rounded-md"
-              fill
-              src={`${backendImageUrl}/${item.image}`}
-              alt=""
-            />
-          </div>
+          <Dialog key={idx}>
+            <DialogTrigger>
+              <div
+                onClick={() => setSelected(idx)}
+                className={clsx(
+                  "relative cursor-pointer w-[180px] h-[180px]  md:h-[200px] md:w-[200px] rounded-md bg-slate-50",
+                )}
+              >
+                <Image
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  className="w-full   h-full object-cover rounded-md"
+                  fill
+                  src={`${backendImageUrl}/${item.image}`}
+                  alt=""
+                />
+              </div>
+            </DialogTrigger>
+            <DialogContent className="min-w-[300px] rounded-md md:min-w-[500px] lg:min-w-[1200px] h-auto  ">
+              <DialogHeader>
+                <DialogTitle className="opacity-0">Gallery modal</DialogTitle>
+                <DialogDescription className="opacity-0">
+                  There you can see selected image
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex items-center justify-center">
+                <div
+                  onClick={() => setSelected(idx)}
+                  className={clsx(
+                    "relative w-[290px] h-[290px] sm:w-[320px] sm:h-[320px] md:w-[420px] md:h-[420px]  lg:w-[520px] lg:h-[520px]",
+                  )}
+                >
+                  <Image
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    className="w-full h-full object-cover rounded-md"
+                    fill
+                    src={`${backendImageUrl}/${item.image}`}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </DialogContent>
+          </Dialog>
         ))}
       </div>
     </div>
