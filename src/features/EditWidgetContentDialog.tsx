4cf055fff457594c9db21dbc9dfd3a0c5f2f7802
iufb <bashirov3ld2@gmail.com>
2024-08-02 16:40:17 +0500

    fix:locale close btn

--- src/features/EditWidgetContentDialog.tsx
+++ src/features/EditWidgetContentDialog.tsx
@@ -61,7 +61,7 @@ export const EditWidgetContentDialog = ({
 };
 
 const CloseButton = ({ close }: { close: () => void }) => {
-  const t = useTranslations("pages.pageEditorContent");
+  const t = useTranslations("pages.pageEditorContent.edit");
   return (
     <AlertDialog>
       <AlertDialogTrigger className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
