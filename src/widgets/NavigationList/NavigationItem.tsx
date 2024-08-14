b855961e1d11305d418a7190e6a8aaf303bda42c
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-08-14 15:06:43 +0500

    update: Добавлены кнопки редактирования и удаления

--- src/widgets/NavigationList/NavigationItem.tsx
+++ src/widgets/NavigationList/NavigationItem.tsx
@@ -1,14 +1,30 @@
+'use client';
 import { DragEvent } from "react";
 import { INavigation } from "./model/Navigation.model";
+import { NavigationEditModal } from "@/features/Modals/NavigationEditModal/NavigationEditModal";
+import { useParams } from "next/navigation";
+import { DeletePageBtn } from "@/features";
 
-export const NavigationItem = ({ item, onDragEnd }: { item: INavigation; onDragEnd: (e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>, draggedItem: INavigation, targetItem: INavigation) => void }) => {
+export const NavigationItem = (
+    {
+        item,
+        onDragEnd
+    }: {
+        item: INavigation;
+        onDragEnd: (e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>,
+            draggedItem: INavigation,
+            targetItem: INavigation
+        ) => void
+    }) => {
+    const locale = useParams().locale as string;
+
     const handleDragStart = (e: React.DragEvent<HTMLLIElement>, navItem: INavigation) => {
         e.stopPropagation();
         e.dataTransfer.setData("text/plain", JSON.stringify(navItem));
     };
 
+
     const handleDrop = (e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>, targetNavItem: INavigation) => {
-        console.log(targetNavItem)
         e.stopPropagation();
         e.preventDefault();
 
@@ -16,33 +32,45 @@ export const NavigationItem = ({ item, onDragEnd }: { item: INavigation; onDragE
         onDragEnd(e, draggedItem, targetNavItem);
     };
 
+
     return (
         <li
             draggable
             onDragStart={(e) => handleDragStart(e, item)}
             onDrop={(e: React.DragEvent<HTMLLIElement>) => handleDrop(e, item)}
             onDragOver={(e) => e.preventDefault()}
-            className={"p-4 border border-gray-200 rounded-md"}
+            className={"p-4 border border-gray-200 rounded-md flex flex-col gap-2"}
         >
-            {item.title["kz"]}
+            <section className="flex items-center">
+                <h3 className="grow">
+                    {item.title[locale]}
 
-            {item.children && item.children.length > 0 ? (
-                <ul className="flex flex-col gap-3">
-                    {item.children.map((child) => (
-                        <NavigationItem key={child.id} item={child} onDragEnd={onDragEnd} />
-                    ))}
-                </ul>
-            ) : (
-                item.navigation_type === 'group' && (
-                    <div
-                        onDrop={(e: DragEvent<HTMLDivElement>) => handleDrop(e, item)}
-                        onDragOver={(e) => e.preventDefault()}
-                        className="p-4 mt-2 border border-dashed border-gray-300 rounded-md"
-                    >
-                        Перетащите сюда элемент, чтобы добавить
-                    </div>
-                )
-            )}
+                </h3>
+                <div className="flex gap-2 ">
+                    <NavigationEditModal navigationItem={item} />
+                    <DeletePageBtn navigationId={item.id} name={item.title[locale]} />
+                </div>
+            </section>
+            <section>
+
+                {item.children && item.children.length > 0 ? (
+                    <ul className="flex flex-col gap-3">
+                        {item.children.map((child) => (
+                            <NavigationItem key={child.id} item={child} onDragEnd={onDragEnd} />
+                        ))}
+                    </ul>
+                ) : (
+                    item.navigation_type === 'group' && (
+                        <div
+                            onDrop={(e: DragEvent<HTMLDivElement>) => handleDrop(e, item)}
+                            onDragOver={(e) => e.preventDefault()}
+                            className="p-4 mt-2 border border-dashed border-gray-300 rounded-md"
+                        >
+                            Перетащите сюда элемент, чтобы добавить
+                        </div>
+                    )
+                )}
+            </section>
         </li>
     );
 };
