0578e687d8a7e198596992c3a8c8d1b297e8499b
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-08-14 16:57:13 +0500

    feat(options): Подготовка странцы для управление контентом страницы

--- src/app/[locale]/admin/(withSidebar)/pages/[id]/page.tsx
+++ src/app/[locale]/admin/(withSidebar)/pages/[id]/page.tsx
@@ -1,16 +1,13 @@
-import { CreatePageDialog, withNoSSR } from "@/features";
-import { getPagesChildren } from "@/shared/api/pages";
-import { PagesListTable } from "@/widgets";
 import { getTranslations } from "next-intl/server";
 interface PageProps {
-  params: { id: string };
-  searchParams: { ruId: number; kzId: number };
+  params: { id: string, locale: string }
 }
-export default async function Page({ params, searchParams }: PageProps) {
+export default async function Page({ params }: PageProps) {
+  console.log(params.id);
   const t = await getTranslations("pages.id");
   return (
     <section>
-      <section className="flex gap-4">
+      {/* <section className="flex gap-4">
         <CreatePageDialog
           ruParentId={searchParams.ruId}
           kzParentId={searchParams.kzId}
@@ -18,7 +15,7 @@ export default async function Page({ params, searchParams }: PageProps) {
         />
       </section>
       <h2 className="text-center text-xl font-bold">{t("title")}</h2>
-      <PagesListTable ids={searchParams} />
+      <PagesListTable ids={searchParams} /> */}
     </section>
   );
 }
