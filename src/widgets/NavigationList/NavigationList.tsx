fc739a7c91678ad66c2e469dd8fb8fb9ea2c5b59
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-08-14 15:07:49 +0500

    fix: Исправлен баг с переносом на 1 уровень навигаций элемента из 2 уровня, когда на 1 нет обычного контентного элемента

--- src/widgets/NavigationList/NavigationList.tsx
+++ src/widgets/NavigationList/NavigationList.tsx
@@ -4,11 +4,8 @@ import { INavigation, INavListUpdateOrderOptions } from "./model/Navigation.mode
 import { NavigationItem } from "./NavigationItem";
 
 
-
-
 export const NavigationList = () => {
   const queryClient = useQueryClient();
-
   const { mutate } = useMutation({
     mutationFn: async (options: INavListUpdateOrderOptions[]) => {
       const response = await fetch("http://localhost:3003/navigations/orders/update", {
@@ -27,20 +24,48 @@ export const NavigationList = () => {
     },
   })
 
+  const handleContainerDragEnd = (e: React.DragEvent<HTMLUListElement>) => {
+    const draggedItem = JSON.parse(e.dataTransfer.getData("text/plain"));
+    console.log(draggedItem)
+    // Если событие drop произошло вне зоны списка, parent_id у элемента будет null
+
+  };
+
 
   const handleDragEnd = (e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>, draggedItem: INavigation, targetItem: INavigation) => {
     e.stopPropagation();
-    console.log('target ', targetItem)
-    console.log('dragged ', draggedItem)
-    if (draggedItem.parent_id === targetItem.parent_id && targetItem.navigation_type === 'content') {
-      mutate([{ id: draggedItem.id, order: targetItem.order }, { id: targetItem.id, order: draggedItem.order }])
-    } else if (targetItem.navigation_type === 'group') {
-      mutate([{ id: draggedItem.id, order: 1, parent_id: targetItem.id }])
+
+    // Оба элемента на одном уровне и targetItem - content
+    if (
+      draggedItem.parent_id === targetItem.parent_id &&
+      targetItem.navigation_type === "content"
+    ) {
+      mutate([
+        { id: draggedItem.id, order: targetItem.order },
+        { id: targetItem.id, order: draggedItem.order },
+      ]);
     }
+    // Дроп за пределы текущего контейнера
+    else if (e.relatedTarget && !e.currentTarget.contains(e.relatedTarget as Node)) {
+      mutate([{ id: draggedItem.id, order: 1, parent_id: null }]);
+    }
+    // Дроп в группу
+    else if (
+      targetItem.navigation_type === "group" &&
+      draggedItem.parent_id !== targetItem.id
+    ) {
+      const newOrder = (targetItem.children ? targetItem.children.length : 0) + 1;
+      mutate([{ id: draggedItem.id, order: newOrder, parent_id: targetItem.id }]);
+    }
+    // Любое другое перемещение (между элементами разных родителей)
     else {
-      mutate([{ id: draggedItem.id, order: targetItem.order, parent_id: targetItem.parent_id }])
+      mutate([
+        { id: draggedItem.id, order: targetItem.order, parent_id: targetItem.parent_id },
+      ]);
     }
   };
+
+
 
   const { data, isLoading } = useQuery<INavigation[]>({
     queryKey: ["navigations"],
@@ -55,11 +80,10 @@ export const NavigationList = () => {
   }
 
   return (
-    <ul className="flex flex-col gap-3">
+    <ul className="flex flex-col gap-3" >
       {
         data?.map((item) => <NavigationItem key={item.id} item={item} onDragEnd={handleDragEnd} />)
       }
     </ul>
   );
 };
-
