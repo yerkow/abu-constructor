40240b6b1cb1589576629af1eb8e0fa40f45c8d8
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2024-09-15 19:50:32 +0500

    update: экспорт дефольных типов

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -9,9 +9,28 @@ export interface Content {
   widget_id: number;
 }
 
-
 export interface IWidgetProps {
   contents: Array<any>;
   options: any;
   locale: string;
+}
+
+export interface IContent {
+  id: number;
+  content: {
+    [key: string]: Object;
+  };
+  options: {
+    [key: string]: Object;
+  };
+}
+
+export interface IWidget {
+  id: number;
+  widget_type: string;
+  options: {
+    [key: string]: any;
+  };
+  order: number;
+  contents: IContent[];
 }
