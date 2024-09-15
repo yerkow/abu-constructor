a77c7a6b37c984340109491fc0a0ac0d8c2efb9e
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2024-09-15 19:55:06 +0500

    change: Убрана страница template

--- src/widgets/AdminSidebar/AdminSidebar.tsx
+++ src/widgets/AdminSidebar/AdminSidebar.tsx
@@ -1,16 +1,13 @@
-import { Button } from "@/shared/ui";
 import clsx from "clsx";
 import { ComponentProps } from "react";
 import { Navlink } from "./Navlink";
 import { ChangeLocale, LogoutButton } from "@/features";
 import { getTranslations } from "next-intl/server";
-import { cookies } from "next/headers";
 
 const links = [
   { label: "links.home.label", href: "links.home.href" },
   { label: "links.settings.label", href: "links.settings.href" },
   { label: "links.pages.label", href: "links.pages.href" },
-  { label: "links.templates.label", href: "links.templates.href" },
 ];
 export const AdminSidebar = async ({
   className,
@@ -22,7 +19,7 @@ export const AdminSidebar = async ({
     <nav
       className={clsx(
         className,
-        "flex z-10 items-center md:items-start md:static flex-row md:flex-col fixed left-0 right-0 h-20 justify-center md:justify-between gap-3 bg-[#690000] text-white p-10 md:h-full",
+        "flex z-10 items-center md:items-start md:static flex-row md:flex-col fixed left-0 right-0 h-20 justify-center md:justify-between gap-3 bg-[#690000] text-white p-10 md:h-full"
       )}
       {...props}
     >
