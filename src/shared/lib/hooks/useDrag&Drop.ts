ed1155e257996e8cbaaca6727985f200817eb2bd
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2024-09-15 18:20:26 +0500

    update: Переписал новый рабочий drag and drop

--- src/shared/lib/hooks/useDrag&Drop.ts
+++ src/shared/lib/hooks/useDrag&Drop.ts
@@ -1,93 +1,59 @@
-import { UseMutateFunction, useMutation, QueryKey } from "@tanstack/react-query";
-import { queryClient } from "../client";
-import { backendUrl } from "../constants";
+import { UseMutateFunction } from "@tanstack/react-query";
 
+export type DragAndDropProps<Item> = (
+  e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>,
+  draggedItem: Item,
+  targetItem: Item,
+  handler: UseMutateFunction<() => Promise<any>, Error, any[], unknown>
+) => void;
 
-export interface ItemUpdateOrderOption {
-    id: number;
-    order: number;
+interface IDragAndDropReturnProps<Item> {
+  handleDrop: (
+    e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>,
+    targetNavItem: Item,
+    handler: UseMutateFunction<() => Promise<any>, Error, any[], unknown>
+  ) => void;
+  handleDragStart: (e: React.DragEvent<HTMLLIElement>, navItem: Item) => void;
 }
 
+/**
+ * @brief: Хук для реализации начальной функциональности drag & drop.
+ *
+ * @Подробное описание:
+ * Хук реализует 2 события:
+ * 1. Обработчик начала перетаскивания:
+ * 	При начале перетаскивания в dataTransfer записывается JSON-строка с данными элемента.
+ *
+ * 2. Обработчик завершения перетаскивания:
+ * 	При завершении перетаскивания из dataTransfer считывается JSON-строка с данными.
+ * 	элемента и вызывается функция onDragEnd, которая обрабатывает перемещение элемента исходя из его исходного и конечного положения.
+ */
 
-interface DragAndDropProps {
-    queryKey: QueryKey,
-    key: string
-}
+export const useDragAndDrop = <Item>(
+  onDragEnd: DragAndDropProps<Item>
+): IDragAndDropReturnProps<Item> => {
+  const handleDragStart = (
+    e: React.DragEvent<HTMLLIElement>,
+    navItem: Item
+  ) => {
+    e.stopPropagation();
+    e.dataTransfer.setData("text/plain", JSON.stringify(navItem));
+  };
 
-export const useDragAndDrop = <Item extends { id: number; order: number }>(
-    { queryKey, key }: DragAndDropProps
-) => {
+  const handleDrop = (
+    e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>,
+    targetNavItem: Item,
+    handler: UseMutateFunction<() => Promise<any>, Error, any[], unknown>
+  ) => {
+    e.stopPropagation();
+    e.preventDefault();
 
-    const handleDragEnd = (
-        e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>,
-        draggedItem: Item,
-        targetItem: Item,
-        handler: UseMutateFunction<() => Promise<any>, Error, ItemUpdateOrderOption[], unknown>
-    ) => {
-        e.stopPropagation();
+    const draggedItem: Item = JSON.parse(e.dataTransfer.getData("text/plain"));
+    onDragEnd(e, draggedItem, targetNavItem, handler);
+  };
 
-        if (draggedItem.id !== targetItem.id) {
-            handler([
-                { id: draggedItem.id, order: targetItem.order },
-                { id: targetItem.id, order: draggedItem.order },
-            ]);
-        }
-    };
-
-    const handleDrop = (
-        e: React.DragEvent<HTMLDivElement> | React.DragEvent<HTMLLIElement>,
-        targetNavItem: Item,
-        handler: UseMutateFunction<any, Error, ItemUpdateOrderOption[], unknown>
-    ) => {
-        e.stopPropagation();
-        e.preventDefault();
-
-        const draggedItem: Item = JSON.parse(e.dataTransfer.getData("text/plain"));
-        handleDragEnd(e, draggedItem, targetNavItem, handler);
-    };
-
-    const handleDragStart = (
-        e: React.DragEvent<HTMLLIElement>,
-        navItem: Item
-    ) => {
-        e.stopPropagation();
-        e.dataTransfer.setData("text/plain", JSON.stringify(navItem));
-    };
-
-    const fetchWidgetOrderUpdate = async (
-        options: ItemUpdateOrderOption[]
-    ) => {
-        const response = await fetch(`${backendUrl}/${key}/v2/orders/update`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(options),
-        });
-
-        if (!response.ok) {
-            throw new Error("Failed to update widget order");
-        }
-
-        return response.json();
-    }
-
-    const { mutate: handleItemUpdate } = useMutation<void, Error, ItemUpdateOrderOption[]>({
-        mutationFn: fetchWidgetOrderUpdate,
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey,
-            });
-        },
-        onError: (error) => {
-            console.error("Error updating widget order:", error);
-        },
-    })
-
-    return {
-        handleDragEnd,
-        handleDrop,
-        handleDragStart,
-        handleItemUpdate
-    }
-}
+  return {
+    handleDrop,
+    handleDragStart,
+  };
+};
