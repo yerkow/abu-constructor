e0983e54e487b88896cbe392e57ffb7764030c58
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-09-17 13:51:09 +0500

    ref: Рефактор компоненты

--- src/entities/Card/ui/CardBase&Horizontal.tsx
+++ src/entities/Card/ui/CardBase&Horizontal.tsx
@@ -2,103 +2,107 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { backendImageUrl, backendUrl } from "@/shared/lib/constants";
+import { backendImageUrl } from "@/shared/lib/constants";
 import { cn } from "@/shared/lib/utils";
 
 export const CardBaseAndHorizontal = ({
   content,
   locale,
   variant,
-  currentPath,
   size = "normal",
+  currentPath,
 }: {
   content: any;
   variant: string;
   locale: string;
-  currentPath: string;
   size: string;
+  currentPath: string;
 }) => {
-  const { title, content: text } = content[locale];
+  const { title, description } = content[locale];
+  const hasDescription = Boolean(description && description.length > 0);
 
-  const Component = content.file
-    ? (Link as React.ElementType)
+  const variantClasses = {
+    base: "flex-1 min-w-[300px]",
+    with_file: "flex-1 min-w-[300px]",
+    with_modal: "flex-1 min-w-[300px]",
+    horizontal: "col-start-1 col-end-2",
+  }[variant];
+
+  const sizeClasses = {
+    normal: "h-[200px]",
+    medium: "h-[350px]",
+    large: "h-[550px]",
+  }[size];
+
+  const WrapperComponent = content.file || content.link ? (Link as React.ElementType)
     : ("div" as "div");
-
-  console.log(variant)
+  const linkProps = content.file
+    ? { href: `${backendImageUrl}/${content.file}`, target: "_blank" }
+    : content.link
+      ? { href: `${content.link}` }
+      : {};
 
   return (
-    <article
-      className={cn(
-        {
-          base: "flex-1 min-w-[300px]",
-          with_file: "flex-1 min-w-[300px]",
-          with_modal: "flex-1 min-w-[300px]",
-          horizontal: "col-start-1 col-end-2",
-        }[variant]
-      )}
-    >
-      <Component
-        {...(content.file && { href: `${backendImageUrl}/${content.file}`, target: "_blank" })}
-        className={cn(
-          "block after:rounded-md after:absolute rounded-2xl relative overflow-hidden shadow-md"
-        )}
+    <article className={cn(variantClasses, "rounded-md")}>
+      <WrapperComponent
+        {...linkProps}
+        className="block after:rounded-md after:absolute rounded-2xl relative overflow-hidden shadow-md"
       >
-        <div
+        {renderContent()}
+      </WrapperComponent>
+    </article>
+  );
+
+  function renderContent() {
+    return (
+      <div className={cn(sizeClasses)}>
+        <section
           className={cn(
-            {
-              normal: "h-[200px]",
-              medium: "h-[350px]",
-              large: "h-[550px]",
-            }[size],
-            {
-              true: "flex items-center justify-center text-white",
-              false: "text-black",
-            }[String(text == undefined)],
-            "relative"
+            hasDescription ? "h-[70%]" : "h-[100%]",
+            "w-full relative"
           )}
-          style={{
-            backgroundImage: !text
-              ? `url('${backendImageUrl}${content.image}')`
-              : "",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-          }}
         >
-          {variant === "base" && !text ? (
+          {content.image ? (
             <>
-              <div className="absolute inset-0 bg-black opacity-35"></div>
-              <h2 className="font-bold text-xl relative px-2">{title}</h2>
-            </>
-          ) : (
-            <div>
               <Image
                 src={`${backendImageUrl}${content.image}`}
                 fill
                 objectFit="cover"
                 objectPosition="top"
                 alt="image"
-                className="absolute left-0 right-0 top-0 bottom-0"
+                className="rounded-md"
               />
-            </div>
+              {!hasDescription && (
+                <>
+                  <div className="absolute inset-0 bg-black opacity-40"></div>
+                  <h2 className="text-white font-bold text-xl relative px-2 flex w-full h-full items-center justify-center">
+                    {title}
+                  </h2>
+                </>
+              )}
+            </>
+          ) : (
+            <div className="absolute inset-0 bg-[#640000]"></div>
           )}
-        </div>
-        {text && (
-          <div className="p-4">
+        </section>
+
+        {hasDescription && (
+          <div className="p-3">
             <h2 className="font-bold text-xl">{title}</h2>
             <p
               className="text-justify"
               dangerouslySetInnerHTML={{
                 __html:
                   variant === "horizontal"
-                    ? text
-                    : text?.length > 110
-                      ? `${text.slice(0, 110)}...`
-                      : text,
+                    ? description
+                    : description.length > 110
+                      ? `${description.slice(0, 110)}...`
+                      : description,
               }}
             ></p>
           </div>
         )}
-      </Component>
-    </article>
-  );
+      </div>
+    );
+  }
 };
