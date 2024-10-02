962f57e083222b9640ea9efeef7dc35b189e2d39
Dzhetenbaev Maksat <maxat.dzhetenbaev@gmail.com>
2024-10-02 15:52:14 +0500

    update: Добавлен env конфигурация

--- src/shared/lib/constants.ts
+++ src/shared/lib/constants.ts
@@ -1,4 +1,4 @@
-export const backendImageUrl = `http://77.243.80.138:3003/uploads/`;
+export const backendImageUrl = process.env.NEXT_PUBLIC_BACKEND_URL + "/uploads/";
 // export const backendImageUrl = `http://localhost:3003/uploads/`;
-export const backendUrl = "http://77.243.80.138:3003/api";
+export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL + "/api";
 // export const backendUrl = "http://localhost:3003/api";
